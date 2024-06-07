bdcd8f0d175076f05d2cebbad74a83ee8c7348d0
potatoshowell <makarlarry98@gmail.com>
2024-06-07 13:15:12 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <p className=" text-green-600 text-5xl font-bold">Andrzej Herman</p>
-      <p className="text-xl">Nr albumu: 99999</p>
-      <Link href="/strona">Przejdź na drugą stronę</Link>
-      <div className="mt-2">
-        <Button className="bg-red-500 hover:bg-red-700">Kliknij</Button>
-      </div>
-      <div className="mt-2">
-        <Button className="bg-blue-500 hover:bg-blue-700">
-          Kliknij drugi przycisk
-        </Button>
-      </div>
+      <p className=" text-green-600 text-5xl font-bold">Makar Trukhanov</p>
+      <p className="text-xl">Nr albumu: 119793</p>
     </div>
   );
 }
